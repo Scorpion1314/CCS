727f82c020d10f2e74d85e281084bd31d71448dc
Scorpion1314 <bryanmelvin.foo999999@hotmail.com>
2023-01-08 18:26:46 +0800

    Create building system (scripts)

---
+++ GitProject/Assets/Scripts/BuildingSystem/BuildingSystem.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BuildingSystem : MonoBehaviour
+{
+    // Create static variable for singleton
+    public static BuildingSystem BSystem;
+
+    // Variable for grid
+    public GridLayout gridLayout;
+    private Grid grid;
+
+    // Variable for tilemap
+    [SerializeField] private Tilemap MainTilemap;
+    [SerializeField] private TileBase whiteTile;
+
+    // Variable for building (in this case 3D objects)
+    public GameObject cube;
+    public GameObject sphere;
+
+    private PlaceableObject objectToPlace;
+
+    private void Awake()
+    {
+        BSystem = this;
+        grid = gridLayout.gameObject.GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            InitializeWithObject(cube);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            InitializeWithObject(sphere);
+        }
+
+        if(!objectToPlace)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            objectToPlace.Rotate();
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(CanBePlaced(objectToPlace))
+            {
+                objectToPlace.Place();
+                Vector3Int start = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+                TakeArea(start, objectToPlace.Size);
+            }
+            else
+            {
+                Destroy(objectToPlace.gameObject);
+            }
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            return raycastHit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    public Vector3 SnapCoordinateToGrid(Vector3 position)
+    {
+        Vector3Int cellPos = gridLayout.WorldToCell(position);
+        position = grid.GetCellCenterWorld(cellPos);
+        return position;
+    }
+
+    private static TileBase[] GetTilesBlock(BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+        foreach (var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x, v.y, 0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++; 
+        }
+
+        return array;
+    }
+
+    public void InitializeWithObject(GameObject shape)
+    {
+        Vector3 position = SnapCoordinateToGrid(Vector3.zero);
+
+        GameObject obj = Instantiate(shape, position, Quaternion.identity);
+        objectToPlace = obj.GetComponent<PlaceableObject>();
+        obj.AddComponent<ObjectDrag>();
+    }
+
+    private bool CanBePlaced(PlaceableObject placeableObject)
+    {
+        BoundsInt area = new BoundsInt();
+        area.position = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+        area.size = placeableObject.Size;
+        area.size = new Vector3Int(area.size.x + 1, area.size.y + 1, area.size.z);
+
+        TileBase[] baseArray = GetTilesBlock(area, MainTilemap);
+
+        foreach (var b in baseArray)
+        {
+            if(b == whiteTile)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void TakeArea(Vector3Int start, Vector3Int size)
+    {
+        MainTilemap.BoxFill(start, whiteTile, start.x, start.y, start.x + size.x, start.y + size.y);
+    }
+}
---
+++ GitProject/Assets/Scripts/BuildingSystem/PlaceableObject.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableObject : MonoBehaviour
+{
+    // To check the placed is placed by other object
+    public bool Placed {get; private set; }
+    // To check the size of the building (in this case shape)
+    public Vector3Int Size {get; private set; }
+
+    // List for the vertices of the object
+    private Vector3[] Vertices;
+
+    private void GetColliderVertexPositionsLocal()
+    {
+        BoxCollider Box = gameObject.GetComponent<BoxCollider>();
+        Vertices = new Vector3[4];
+        Vertices[0] = Box.center + new Vector3(-Box.size.x, -Box.size.y, -Box.size.z) * 0.5f;
+        Vertices[1] = Box.center + new Vector3(Box.size.x, -Box.size.y, -Box.size.z) * 0.5f;
+        Vertices[2] = Box.center + new Vector3(Box.size.x, -Box.size.y, Box.size.z) * 0.5f;
+        Vertices[3] = Box.center + new Vector3(-Box.size.x, -Box.size.y, Box.size.z) * 0.5f;
+    }
+
+    private void CalculateSizeInCells()
+    {
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = BuildingSystem.BSystem.gridLayout.WorldToCell(worldPos);
+        }
+
+        Size = new Vector3Int(Math.Abs((vertices[0] - vertices[1]).x), Math.Abs((vertices[0] - vertices[3]).y), 1);
+    }
+
+    public Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(Vertices[0]);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetColliderVertexPositionsLocal();
+        CalculateSizeInCells();
+    }
+
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 90, 0));
+        Size = new Vector3Int(Size.y, Size.x, 1);
+
+        Vector3[] vertices = new Vector3[Vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = Vertices[(i + 1) % Vertices.Length];
+        }
+
+        Vertices = vertices;
+    }
+
+    public virtual void Place()
+    {
+        ObjectDrag Drag = gameObject.GetComponent<ObjectDrag>();
+        Destroy(Drag);
+
+        Placed = true;
+    }
+}
